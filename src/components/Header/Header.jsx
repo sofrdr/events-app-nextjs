028d5a110fd9b08f3800ea49a356be9cd7784e0e
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-02-02 11:34:08 +0100

    Styling header and footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,33 @@
+import Image from "next/image";
 import Link from "next/link";
+import logo from "../../../public/images/logo_black.png";
 
 const Header = () => {
   return (
     <header>
-      <nav>
-        <Link href="/" passHref>
-          Home
-        </Link>
-        <Link href="/about-us" passHref>
-          About us
-        </Link>
-        <Link href="/events" passHref>
-          Events
-        </Link>
-      </nav>
+      <div className="top-nav">
+        <Image src={logo} width={50} height={50} />
+        <nav>
+          <ul>
+            <li>
+              <Link href="/" passHref>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/about-us" passHref>
+                About us
+              </Link>
+            </li>
+            <li>
+              <Link href="/events" passHref>
+                Events
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <h1>Ut enim ad minim veniam</h1>
     </header>
   );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import Layout from "@/components/Layout/Layout";
-import "@/styles/globals.css";
+import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
   return (
