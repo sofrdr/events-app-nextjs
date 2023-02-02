98133d4b1e37f620f60f26e117d419703ca372ae
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-02-02 12:54:55 +0100

    Styling events page

--- src/components/Events/AllEvents.jsx
+++ src/components/Events/AllEvents.jsx
@@ -4,20 +4,17 @@ import Link from "next/link";
 
 export default function AllEvents({ data }) {
   return (
-    <div>
-      <h1>Events Page</h1>
+    <div className="events-body">
       {data.map((event) => {
         const { id, title, description, image } = event;
         return (
           <Link className="card" key={id} href={`/events/${id}`} passHref>
             <div className="card-image">
-              <Image src={image} alt={title} width={400} height={300} />
+              <Image src={image} alt={title} width={350} height={350} />
             </div>
 
-            <div className="card-content">
-              <h2>{title}</h2>
-              <p>{description}</p>
-            </div>
+            <h2>{title}</h2>
+            <p>{description}</p>
           </Link>
         );
       })}
